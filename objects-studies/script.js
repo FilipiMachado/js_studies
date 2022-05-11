c72090537a4b276bd0f870c2bb37e015bf9237c3
FilipiMachado <filipisee@hotmail.com>
2022-05-10 23:18:50 -0300

    working with objects

--- objects-studies/script.js
+++ objects-studies/script.js
@@ -1,1 +1,20 @@
-console.log("hello world!")
+let person = {
+  name: ['Bob', 'Smith'],
+  age: 32,
+  gender: 'male',
+  interests: ['dance', 'music'],
+  bio: function() {
+    console.log(`${this.name[0]} ${this.name[1]} have ${this.age} years old. He likes to ${this.interests[0]} and listen to ${this.interests[1]}.`)
+  }
+}
+
+/* person.bio()
+console.log(person.age) */
+
+console.log(person)
+
+person.hello = function() { alert('Goodbye to you all!') }
+
+console.log(person)
+
+person.hello()
