ca5a88e842ba0e94ac100ff45b5c07b17f727f0c
FilipiMachado <filipisee@hotmail.com>
2022-05-11 16:17:33 -0300

    changing flex direction from getElementById

--- objects-studies/script.js
+++ objects-studies/script.js
@@ -1,15 +1,76 @@
+function Person(name, age) {
+  this.name = name
+  this.age = age
+  this.greeting = function () {
+    console.log(`Hi! I'm ${this.name}. I have ${this.age} years old!`)
+  }
+}
+
+let person1 = new Person('Fil', 35)
+let person2 = new Person('Vitrola', 21)
+
+console.log(person1)
+person1.greeting()
+console.log(person2)
+person2.greeting()
+
+/* let fil = createNewPerson('Fil', 35)
+let vitrola = createNewPerson('Vitrola', 21)
+
+console.log(fil)
+console.log(vitrola)
+
+fil.greeting()
+vitrola.greeting()
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /* let person = {
-  name: ['Bob', 'Smith'],
+  name: {
+    first: 'Bob',
+    last: 'Smith'
+  },
   age: 32,
   gender: 'male',
   interests: ['dance', 'music'],
   bio: function() {
-    console.log(`${this.name[0]} ${this.name[1]} have ${this.age} years old. He likes to ${this.interests[0]} and listen to ${this.interests[1]}.`)
+    console.log(`${this.name.first} ${this.name.last} have ${this.age} years old. He likes to ${this.interests[0]} and listen to ${this.interests[1]}.`)
   }
 }
- */
 
-let isRow = false;
+person.bio() */
+
+/* let myDataName = 'tall';
+let myDataValue = '1.75m';
+person[myDataName] = myDataValue;
+
+console.log(myDataName) */
+
+/* person.eyes = 'brown'
+person.congrats = function () {console.log('Congratulations to you ' + person.name[0] + "!")}
+
+console.log(person.eyes)
+person.congrats() */
+
+/* let isRow = false;
 let myContainer = document.getElementById("flex-container");
 
 document.getElementById("change-flex-btn").addEventListener("click", changeFlexDirection);
@@ -23,7 +84,7 @@ function changeFlexDirection() {
     myContainer.style.flexDirection = "column";
     isRow = true
 }
-}
+} */
 
 /* let text = ""
 const fruits = ['orange', 'apple', 'strawberry']
