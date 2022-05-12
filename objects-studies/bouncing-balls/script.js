abdc7e75d2c124a5fd0e2bdb907a79d5ba8163a8
FilipiMachado <filipisee@hotmail.com>
2022-05-12 00:01:04 -0300

    working with bouncing balls objects

--- objects-studies/bouncing-balls/script.js
+++ objects-studies/bouncing-balls/script.js
@@ -18,3 +18,23 @@ function random(min, max) {
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
+
+// Modelling the ball
+
+function Ball(x, y, velX, velY, color, size) {
+  this.x = x;
+  this.y = y;
+  this.velX = velX;
+  this.velY = velY;
+  this.color = color;
+  this.size = size;
+}
+
+// Drawing the ball
+
+Ball.prototype.draw = function () {
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.fill();
+};
