a94c108a3bb76999879e2f80d33894e306415e8f
FilipiMachado <filipisee@hotmail.com>
2022-05-12 16:38:50 -0300

    finished 2nd exercise

--- objects-studies/object-exercises/script.js
+++ objects-studies/object-exercises/script.js
@@ -27,3 +27,15 @@ var student = {
 name : "David Rayy",
 sclass : "VI",
 rollno : 12 }; */
+
+const student = {
+  name: "David Rayy",
+  sclass: "VI",
+  rollno: 12,
+};
+
+console.log(student)
+
+delete student.rollno
+
+console.log(student)
