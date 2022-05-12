00de3839ceda0038e3cd91d4ee15fa17724f4bfc
FilipiMachado <filipisee@hotmail.com>
2022-05-12 14:48:32 -0300

    working with object methods

--- objects-studies/modern-js-objects/script.js
+++ objects-studies/modern-js-objects/script.js
@@ -1,19 +1,45 @@
 // object literals
 
+const blogs = [
+  {
+    title: 'amazing movies to watch',
+  },
+  {
+    title: 'new things to discover',
+  },
+  {
+    title: 'some variety of food',
+  },
+];
+
+console.log(blogs)
+
 let user = {
   name: 'Fil',
   age: 25,
   email: 'filbr@myemail.com',
   blogs: ['amazing movies to watch', 'new things to discover', 'some variety of food'],
-  login: function (info) {
+  login(info) {
     console.log(`The user: ${info} is logged in!`)
   },
-  showBlogs: function () {
+  showBlogs() {
     
+    //console.log(this)
+    /* console.log(`The user has written the following blogs: `)
+    this.blogs.forEach(blogs => {
+      console.log('- ' + blogs)
+    }) */
   }
 }
 
-let newUserName = 'Vitrola'
+
+/* let helloMessage = () => {
+  console.log('message')
+}
+
+helloMessage()
+ */
+/* let newUserName = 'Vitrola'
 newUserName.toUpperCase()
 
-user.login(newUserName)
+user.login(newUserName) */
