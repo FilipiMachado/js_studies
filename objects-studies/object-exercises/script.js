d605746ee5bb11157d5365cc89d3acd0544753de
FilipiMachado <filipisee@hotmail.com>
2022-05-12 16:34:58 -0300

    finished first exercise

---
+++ objects-studies/object-exercises/script.js
@@ -0,0 +1,29 @@
+/* 1. Write a JavaScript program to list the properties of a JavaScript object.
+
+  Sample object:
+  var student = {
+  name : "David Rayy",
+  sclass : "VI",
+  rollno : 12 };
+
+Sample Output: name,sclass,rollno 
+
+
+let student = {
+  name: "David Rayy",
+  sclass: "VI",
+  rollno: 12,
+};
+
+for (data in student) {
+  console.log(data)
+} */
+
+//-------------------------------------------------------------------------------------------------------------------
+
+/* 2. Write a JavaScript program to delete the rollno property from the following object. Also print the object before or after deleting the property. Go to the editor
+Sample object:
+var student = {
+name : "David Rayy",
+sclass : "VI",
+rollno : 12 }; */
