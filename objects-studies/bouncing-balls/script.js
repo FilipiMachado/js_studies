897b2412d2b8b76746756efc7100e9dfe3fe1213
FilipiMachado <filipisee@hotmail.com>
2022-05-12 11:01:11 -0300

    structuring the bouncing ball

--- objects-studies/bouncing-balls/script.js
+++ objects-studies/bouncing-balls/script.js
@@ -1,6 +1,7 @@
 // setup canvas
 
 const canvas = document.querySelector("canvas");
+
 const ctx = canvas.getContext("2d");
 
 const width = (canvas.width = window.innerWidth);
@@ -11,13 +12,13 @@ const height = (canvas.height = window.innerHeight);
 function random(min, max) {
   const num = Math.floor(Math.random() * (max - min + 1)) + min;
   return num;
-}
+};
 
 // function to generate random color
 
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
-}
+};
 
 // Modelling the ball
 
@@ -28,7 +29,7 @@ function Ball(x, y, velX, velY, color, size) {
   this.velY = velY;
   this.color = color;
   this.size = size;
-}
+};
 
 // Drawing the ball
 
@@ -37,4 +38,54 @@ Ball.prototype.draw = function () {
   ctx.fillStyle = this.color;
   ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
   ctx.fill();
+};
+
+let testBall = new Ball(50, 100, 4, 4, 'red', 15);
+
+console.log(testBall)
+
+testBall.x
+testBall.size
+testBall.color
+testBall.draw()
+
+// Adding collision to the ball
+
+Ball.prototype.update = function() {
+  if ((this.x + this.size) >= width) {
+    this.velX = -(this.velX);
+  }
+
+  if ((this.x - this.size) <= 0) {
+    this.velX = -(this.velX);
+  }
+
+  if ((this.y + this.size) >= height) {
+    this.velY = -(this.velY);
+  }
+
+  if ((this.y - this.size) <= 0) {
+    this.velY = -(this.velY);
+  }
+
+  this.x += this.velX;
+  this.y += this.velY;
+};
+
+let balls = [];
+
+while (balls.length < 25) {
+  let size = random(10,20);
+  let ball = new Ball(
+    // ball position always drawn at least one ball width
+    // away from the edge of the canvas, to avoid drawing errors
+    random(0 + size,width - size),
+    random(0 + size,height - size),
+    random(-7,7),
+    random(-7,7),
+    'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
+    size
+  );
+
+  balls.push(ball);
 };
